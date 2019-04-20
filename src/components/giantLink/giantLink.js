5c06d717933b0d8a2288bf96e202253e871cc7c2
Robert Tyree <tyreer@gmail.com>
2019-04-20 20:29:27 +0100

    wip

--- src/components/giantLink/giantLink.js
+++ src/components/giantLink/giantLink.js
@@ -3,45 +3,59 @@ import "./giantLink.css";
 import { useTrail, animated, config, useSpring } from "react-spring";
 
 const rotations = [
-  { id: 0, rotate: "-45deg" },
-  { id: 1, rotate: "30deg" },
-  { id: 2, rotate: "-20deg" }
+  { id: 0, rotate: "20deg" },
+  { id: 1, rotate: "15eg" },
+  { id: 2, rotate: "10deg" }
 ];
 
 const GiantLink = ({ activeLink = 0 }) => {
-  // const letters = activeLink.text.split("");
+  const letters = activeLink.text.split("");
 
-  const [offset, set] = useSpring(() => ({
-    from: { transform: "translate(-50%, -50%) scale(0.4) rotate(-10deg)" },
-    to: {
-      transform: `translate(-50%, -50%) scale(1) rotate(${
-        rotations[activeLink.id - 1].rotate
-      })`
-    },
-    config: config.stiff
-  }));
+  // const [offset, set] = useSpring(() => ({
+  //   from: { transform: "translateX(-20vw) rotate(-1deg)" },
+  //   to: {
+  //     transform: `translateX(0) rotate(${rotations[activeLink.id - 1].rotate})`
+  //   },
+  //   config: config.stiff
+  // }));
 
-  // const trail = useTrail(letters.length, {
-  //   config: { mass: 1, tension: 200, friction: 15 },
-  //   from: { transform: "scale(0) rotate(80deg)" },
-  //   to: { transform: "scale(1) rotate(0)" }
-  // });
+  const trail = useTrail(letters.length, {
+    // config: { mass: 1, tension: 200, friction: 15 },
+    config: config.stiff,
+    from: { transform: "translateY(50vh)", opacity: "0" },
+    transform: "translateY(-5vh)",
+    opacity: 1
+  });
 
-  const wordArr = [];
-  for (let x = 0; x < 4; x++) {
-    wordArr.push(activeLink.text);
-  }
+  // const wordArr = [];
+  // for (let x = 0; x < 1; x++) {
+  //   wordArr.push(activeLink.text);
+  // }
 
   return (
     <>
-      <animated.div
-        className={`bg bg-${activeLink.id}`}
-        style={{ transform: offset.transform }}
-      >
+      {/* <animated.div style={offset} className={`bg bg-${activeLink.id}`}>
         {wordArr.map((w, index) => (
-          <p key={index}>{w}</p>
+          <span style={{ color: index % 2 === 0 ? "#404b69" : "#f73859" }}>
+            {w}
+          </span>
         ))}
-      </animated.div>
+      </animated.div> */}
+      <div className={"giantContainer"}>
+        {trail.map(({ ...rest }, index) => (
+          <animated.div
+            key={index}
+            style={{
+              ...rest
+            }}
+          >
+            <span style={{ color: index % 2 === 0 ? "#404b69" : "#f73859" }}>
+              {letters[index]}
+            </span>
+          </animated.div>
+        ))}
+      </div>
+
       {/* <div className={"root"}>
         {trail.map(({ ...rest }, index) => (
           <animated.div
