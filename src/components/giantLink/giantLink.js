f728ce414424b0c8e37b437102e7f1018c6b221d
Robert Tyree <tyreer@gmail.com>
2019-04-22 22:33:08 +0100

    add random rotation and offset values

--- src/components/giantLink/giantLink.js
+++ src/components/giantLink/giantLink.js
@@ -2,35 +2,73 @@ import React from "react";
 import "./giantLink.css";
 import { useTrail, animated, config, useSpring } from "react-spring";
 
-const rotations = [
-  { id: 0, rotate: "20deg" },
-  { id: 1, rotate: "15eg" },
-  { id: 2, rotate: "10deg" }
-];
+const getRandomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min) + min);
 
-const GiantLink = ({ activeLink = 0 }) => {
+const GiantLink = ({ activeLink }) => {
   const letters = activeLink.text.split("");
 
-  // const [offset, set] = useSpring(() => ({
-  //   from: { transform: "translateX(-20vw) rotate(-1deg)" },
-  //   to: {
-  //     transform: `translateX(0) rotate(${rotations[activeLink.id - 1].rotate})`
-  //   },
-  //   config: config.stiff
-  // }));
+  let yValue;
 
-  const trail = useTrail(letters.length, {
+  switch (activeLink.id) {
+    case 1:
+      yValue = "translateY(50vh)";
+      break;
+    case 2:
+      yValue = "translateY(-50vh)";
+      break;
+    case 3:
+      yValue = "translateY(50vh)";
+      break;
+  }
+
+  const trailLength = getRandomInt(1, 4);
+
+  const trail = useTrail(trailLength, {
     // config: { mass: 1, tension: 200, friction: 15 },
     config: config.stiff,
-    from: { transform: "translateY(50vh)", opacity: "0" },
-    transform: "translateY(0)",
+    from: { transform: `${yValue} translateX(-50%)`, opacity: "0" },
+    transform: "translateY(0) translateX(-50%)",
     opacity: 1
   });
 
-  // const wordArr = [];
-  // for (let x = 0; x < 1; x++) {
-  //   wordArr.push(activeLink.text);
+  let crazyStyles;
+
+  switch (activeLink.id) {
+    case 1:
+      crazyStyles = {
+        bottom: `${getRandomInt(10, 20)}vh`
+      };
+      break;
+    case 2:
+      crazyStyles = {
+        bottom: `${getRandomInt(20, 40)}vh`,
+        left: ` ${getRandomInt(20, 30)}%`
+      };
+      break;
+    case 3:
+      crazyStyles = {
+        bottom: `${getRandomInt(40, 50)}vh`,
+        left: ` ${getRandomInt(65, 75)}%`
+      };
+      break;
+  }
+
+  // let offset;
+
+  // switch (activeLink.id) {
+  //   case 1:
+  //     offset = 1;
+  //     break;
+  //   case 2:
+  //     offset = -1;
+  //     break;
+  //   case 3:
+  //     offset = 1;
+  //     break;
   // }
+
+  // const offset = activeLink.id === 2 ? -1 : 1;
 
   return (
     <>
@@ -41,17 +79,27 @@ const GiantLink = ({ activeLink = 0 }) => {
           </span>
         ))}
       </animated.div> */}
-      <div className={"giantContainer"}>
-        {trail.map((trailAnimation, index) => (
-          <animated.span
+
+      {trail.map((trailAnimation, index) => (
+        <div
+          style={{
+            ...crazyStyles,
+            transform: `rotate(${getRandomInt(-60, 60)}deg)`
+          }}
+          className="outer"
+        >
+          <animated.div
             key={index}
-            style={trailAnimation}
+            style={{
+              ...trailAnimation,
+              bottom: `${index * 25}px`
+            }}
             className="letterContainer"
           >
-            {letters[index]}
-          </animated.span>
-        ))}
-      </div>
+            {activeLink.text}
+          </animated.div>
+        </div>
+      ))}
 
       {/* <div className={"root"}>
         {trail.map(({ ...rest }, index) => (
